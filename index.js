9f738a1560501fd9cacf83a3e0fb936e3472b7ae
Spooro <52739706+Spooro@users.noreply.github.com>
2024-05-02 01:17:34 -0400

    Create index .js
    animates the nav menu

---
+++ index.js
@@ -0,0 +1,54 @@
+let duration = 600;
+let PAGESmenuShape = $(".menu_shape.pages");
+let PAGESmenuShapeBG = $(".menu_shape-bg.pages");
+let PAGESmenuLink = $(".menu_link.pages");
+let PAGEScurrentLink = $(".menu_link.pages.w--current");
+
+// On Click
+PAGESmenuLink.on("click", function(e) {
+    e.preventDefault();
+    let clickedIndex = $(this).index();
+    let currentIndex = PAGEScurrentLink.index();
+
+    // menuShape move
+    barba.go($(this).attr("href"), "opacity-transition");
+
+    // menuShapeBG Stretch
+    if (clickedIndex > currentIndex) {
+        PAGESmenuShape.css("justify-content", "flex-end");
+    } else {
+        PAGESmenuShape.css("justify-content", "flex-start");
+    }
+
+    if (currentIndex !== clickedIndex) {
+        PAGESmenuShapeBG.css("transition", `width ${duration / 2}ms`);
+        PAGESmenuShapeBG.css("width", "140%");
+        setTimeout(() => {
+            PAGESmenuShapeBG.css("width", "100%");
+        }, duration / 2);
+    }
+
+    PAGESmenuShape.css("transition", `all ${duration}ms`);
+    setTimeout(() => {
+        moveShape($(this));
+    }, 200);
+});
+
+function moveShape(PAGEStarget) {
+    let PAGESlinkWidth = PAGEStarget.innerWidth();
+    let PAGESlinkOffset = PAGEStarget.offset().left;
+    let PAGESmenuOffset = $(".menu.pages").offset().left;
+    let PAGESleftPosition = PAGESlinkOffset - PAGESmenuOffset;
+    PAGESmenuShape.css("transition", `all ${duration + 200}ms`);
+    PAGESmenuShape.css("width", PAGESlinkWidth);
+    PAGESmenuShape.css("left", PAGESleftPosition);
+}
+
+moveShape(PAGEScurrentLink);
+$(".menu_link-bg.pages").css("opacity", "0");
+PAGESmenuShape.css("opacity", "1");
+
+// resize
+window.addEventListener("resize", function() {
+    moveShape(PAGEScurrentLink);
+});
