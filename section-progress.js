d9454824fb9c83513a402dc2fed65546457c0d03
Spooro <52739706+Spooro@users.noreply.github.com>
2024-05-02 15:37:06 -0400

    update

---
+++ section-progress.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    const sections = document.querySelectorAll(`[wb-section]`);
+    sections.forEach((section) => {
+      const sectionName = section.getAttribute("wb-section");
+      const link = document.querySelector(`[wb-nav-link="${sectionName}"]`);
+      if (!link) {
+        console.log("No link found for section:", sectionName);
+        return;
+      }
+  
+      const bg = link.querySelector(".nav-link-bg");
+  
+      // Initial State off to the left
+      gsap.set(bg, { xPercent: -101 });
+  
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        onUpdate: (self) => {
+          const progress = self.progress;
+          const xTranslation = progress * 100 - 101;
+          gsap.set(bg, { xPercent: xTranslation });
+        },
+        onLeave: () => {
+          // End state animate off to right
+          gsap.to(bg, { xPercent: 101, duration: 0.3, ease: "Power3.inOut" });
+        },
+      });
+    });
+  });
---
+++ video-link.js
@@ -0,0 +1,10 @@
+//video link to website
+
+document.getElementById("beautyfloor").addEventListener("click", function () {
+    // Replace "https://example.com" with the URL you want to redirect to
+    window.location.href = "https://bfloor.ru/";
+  });
+  document.getElementById("inksoul").addEventListener("click", function () {
+    // Replace "https://example.com" with the URL you want to redirect to
+    window.location.href = "https://inksoul.webflow.io/";
+  });
