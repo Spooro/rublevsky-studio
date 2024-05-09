4a8e61b05b972bfbd201ed62363aac6b5e4dc7a5
Spooro <52739706+Spooro@users.noreply.github.com>
2024-05-09 14:36:03 -0400

    Update blur-on-scroll.js
    potential fix for blur on scroll and modals?

--- blur-on-scroll.js
+++ blur-on-scroll.js
@@ -1,14 +1,24 @@
 // Check if the screen width is greater than or equal to 768px (typical tablet breakpoint)
 if (window.innerWidth >= 768) {
     let currentPixel = window.pageYOffset;
+    const blurSVG = document.createElement('svg');
+    blurSVG.innerHTML = `
+      <filter id="blur">
+        <feGaussianBlur stdDeviation="0 0" />
+      </filter>
+    `;
+    document.body.appendChild(blurSVG);
+  
     const looper = function() {
       const newPixel = window.pageYOffset;
       const diff = newPixel - currentPixel;
       const speed = diff * 0.1;
       const blur = speed < 0 ? speed * -1 : speed;
-      TweenMax.to("#test", .5, { attr: { stdDeviation: "0" + blur * 1 } });
+      const stdDeviation = `${blur * 1} ${blur * 1}`;
+      blurSVG.firstChild.setAttribute('stdDeviation', stdDeviation);
       currentPixel = newPixel;
       requestAnimationFrame(looper);
     };
+  
     looper();
   }
