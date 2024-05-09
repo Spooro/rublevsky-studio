b3d0e51060931e2d6c8fd179b1d7433034cdfc9c
Spooro <52739706+Spooro@users.noreply.github.com>
2024-05-09 12:23:38 -0400

    Create blur-on-scroll.js

---
+++ blur-on-scroll.js
@@ -0,0 +1,15 @@
+let currentPixel = window.pageYOffset;
+const looper = function() {
+  const newPixel = window.pageYOffset;
+  const diff = newPixel - currentPixel;
+  const speed = diff * 0.1;
+  const blur = speed < 0 ? speed * -1 : speed;
+  TweenMax.to("#test", .5, {
+    attr: {
+      stdDeviation: "0" + blur * 1
+    }
+  });
+  currentPixel = newPixel;
+  requestAnimationFrame(looper);
+};
+looper();
