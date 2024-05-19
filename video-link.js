5813080b7fc02b77fefc993a01255dfbe2ee0a1f
Spooro <52739706+Spooro@users.noreply.github.com>
2024-05-18 22:21:09 -0400

    video link works with attributes now, added APS

--- video-link.js
+++ video-link.js
@@ -1,10 +1,29 @@
-//video link to website
+// Select all elements with the "click-redirect" attribute set to "bfloor"
+const beautyfloorElements = document.querySelectorAll('[click-redirect="bfloor"]');
 
-document.getElementById("beautyfloor").addEventListener("click", function () {
-    // Replace "https://example.com" with the URL you want to redirect to
+// Add click event listener to each element
+beautyfloorElements.forEach(function(element) {
+  element.addEventListener("click", function() {
     window.location.href = "https://bfloor.ru/";
   });
-  document.getElementById("inksoul").addEventListener("click", function () {
-    // Replace "https://example.com" with the URL you want to redirect to
+});
+
+// Select all elements with the "click-redirect" attribute set to "inksoul"
+const inksoulElements = document.querySelectorAll('[click-redirect="inksoul"]');
+
+// Add click event listener to each element
+inksoulElements.forEach(function(element) {
+  element.addEventListener("click", function() {
     window.location.href = "https://inksoul.webflow.io/";
   });
+});
+
+// Select all elements with the "click-redirect" attribute set to "aps"
+const apsElements = document.querySelectorAll('[click-redirect="aps"]');
+
+// Add click event listener to each element
+apsElements.forEach(function(element) {
+  element.addEventListener("click", function() {
+    window.location.href = "https://www.africapowersupply.com/";
+  });
+});
